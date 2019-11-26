25830ed781507eeb579a9518b7df0f5d95b87839
FIC <FIC@info.local>
2019-11-26 14:18:40 -0300

    BD_SQL

--- EX/dadosCadrasto.sql
+++ EX/dadosCadrasto.sql
@@ -225,3 +225,170 @@ SELECT * FROM DEPENDENTE
 SELECT * FROM LOCALIZACAO
 SELECT * FROM PROJETO
 SELECT * FROM REGISTRO_PROJETO
+
+
+/* ATIVIDADE 9 */
+
+CREATE DATABASE bdcamiseta
+USE bdcamiseta;
+
+CREATE TABLE marca(
+	IdMarca INT IDENTITY PRIMARY KEY NOT NULL,
+	Nome VARCHAR(30)
+);
+CREATE TABLE cor(
+	IdCor INT IDENTITY PRIMARY KEY NOT NULL,
+	Nome VARCHAR(30)
+);
+CREATE TABLE tamanho(
+	IdTamanho INT IDENTITY PRIMARY KEY NOT NULL,
+	Tamanho VARCHAR(5)
+);
+
+CREATE TABLE camiseta(
+	IdCamiseta INT IDENTITY PRIMARY KEY NOT NULL,
+	IdMarca INT FOREIGN KEY REFERENCES marca(IdMarca),
+	IdCor INT FOREIGN KEY REFERENCES cor(IdCor),
+	IdTamanho INT FOREIGN KEY REFERENCES tamanho(IdTamanho),
+);
+
+INSERT INTO marca(Nome) VALUES ('Adidas'),('Nike'),('Puma');
+
+INSERT INTO cor(Nome) VALUES ('Preta'),('Branca'),('Azul');
+
+INSERT INTO tamanho(Tamanho) VALUES ('P'),('M'),('G'),('GG');
+
+INSERT INTO camiseta(IdMarca,IdCor,IdTamanho) VALUES (1,2,3),(3,2,3),(2,2,4),(1,3,1),(3,3,3),(1,1,1);
+
+--Alterando nome da primeira marca
+UPDATE marca SET
+	Nome = 'GAP'
+WHERE IdMarca = 1;
+
+--Alterando nome da terceira marca
+UPDATE marca SET
+	Nome = 'Vans'
+WHERE IdMarca = 3;
+
+--Deletando o maior id da tabela camiseta
+DELETE FROM camiseta WHERE IdCamiseta = (SELECT MAX(IdCamiseta) FROM camiseta);
+
+SELECT * FROM camiseta;
+
+SELECT * FROM marca;
+
+SELECT * FROM cor;
+
+SELECT * FROM camiseta
+	INNER JOIN marca ON marca.IdMarca = camiseta.IdMarca
+
+SELECT * FROM camiseta
+	INNER JOIN cor ON cor.IdCor = camiseta.IdCor
+
+SELECT * FROM camiseta
+	INNER JOIN tamanho ON tamanho.IdTamanho = camiseta.IdTamanho
+
+
+ALTER TABLE marca ADD Quant_Loja VARCHAR(20);
+    
+ALTER TABLE marca ADD DataCriacao DATE;
+
+ALTER TABLE camiseta ADD LavarMaquina VARCHAR(20);
+
+ALTER TABLE tamanho ADD Medidas VARCHAR(20);
+
+UPDATE marca SET
+	Quant_Loja = '3005',
+	DataCriacao = '1969-08-21'
+WHERE IdMarca = 1;
+
+UPDATE marca SET
+	Quant_Loja = '4000',
+	DataCriacao = '1964-01-25'
+WHERE IdMarca = 2;
+
+UPDATE marca SET
+	Quant_Loja = '2000',
+	DataCriacao = '1966-03-16'
+WHERE IdMarca = 2;
+
+UPDATE camiseta SET
+	LavarMaquina = 'Sim'
+
+UPDATE tamanho SET
+	Medidas = '30x30'
+WHERE IdTamanho = 1;
+
+UPDATE tamanho SET
+	Medidas = '40x40'
+WHERE IdTamanho = 2;
+
+UPDATE tamanho SET
+	Medidas = '50x50'
+WHERE IdTamanho = 3;
+
+UPDATE tamanho SET
+	Medidas = '60x60'
+WHERE IdTamanho = 4;
+
+
+--ou
+CREATE DATABASE bdcamiseta
+USE bdcamiseta;
+
+CREATE TABLE marca(
+	IdMarca INT IDENTITY PRIMARY KEY NOT NULL,
+	Nome VARCHAR(30)
+);
+CREATE TABLE cores(
+	IdCores INT IDENTITY PRIMARY KEY NOT NULL,
+	Nome VARCHAR(30)
+);
+CREATE TABLE tamanho(
+	IdTamanho INT IDENTITY PRIMARY KEY NOT NULL,
+	Tamanho VARCHAR(5)
+);
+CREATE TABLE camiseta(
+	IdCamiseta INT IDENTITY PRIMARY KEY NOT NULL,
+	IdMarca INT FOREIGN KEY REFERENCES marca(IdMarca),
+	IdCor INT FOREIGN KEY REFERENCES cores(IdCores),
+	IdTamanho INT FOREIGN KEY REFERENCES tamanho(IdTamanho),
+);
+
+
+SELECT MARCA.NOME FROM MARCA
+SELECT * FROM MARCA
+
+SELECT Tamanho.Tamanho FROM tamanho
+SELECT * FROM tamanho
+
+SELECT cores.Nome FROM cores
+SELECT * FROM cores
+
+SELECT * FROM CAMISETA
+
+
+--INNER JOIN
+SELECT CAMISETA.IDCAMISETA, MARCA.NOME FROM CAMISETA
+INNER JOIN MARCA ON MARCA.IDMARCA = CAMISETA.IDMARCA
+
+SELECT CAMISETA.IDCAMISETA, cores.NOME FROM CAMISETA
+INNER JOIN cores ON cores.IdCores = CAMISETA.IDCOR
+
+SELECT CAMISETA.IDCAMISETA, Tamanho.Tamanho FROM CAMISETA
+INNER JOIN tamanho ON Tamanho.IdTamanho = CAMISETA.IDTAMANHO
+
+
+INSERT INTO marca(Nome) VALUES ('Adidas'),('Hollister'),('oakley');
+INSERT INTO cores(Nome) VALUES ('branca'),('vermelha'),('azul');
+INSERT INTO tamanho(Tamanho) VALUES ('P'),('M'),('G'),('GG');
+INSERT INTO camiseta(IdMarca,IdCor,IdTamanho) VALUES (1,2,3),(3,2,3),(2,2,4),(1,3,1),(3,3,3),(1,1,1);
+
+
+--ALTERAÇÃO DE MARCA
+UPDATE MARCA SET NOME='ADIDAS' WHERE IDMARCA= 1 
+UPDATE MARCA SET NOME='DROPDEAD' WHERE IDMARCA= 2
+
+DELETE FROM CAMISETA WHERE IDCAMISETA = (SELECT MAX(IDCAMISETA) FROM CAMISETA)
+
+
--- explicação/DQLs.sql
+++ explicação/DQLs.sql
@@ -8,3 +8,61 @@ SELECT * FROM aluno WHERE Idade = 26;
 
 /* Mostrar dados em uma "Busca" com LIKE -- % ENTRE AS ASPAS */
 SELECT * FROM aluno WHERE Nome LIKE '%Bruna%';
+
+/* Juntando as 3 tabela usando INNER JOIN */
+SELECT * FROM trabalho
+INNER JOIN materia ON materia.IdMateria = trabalho.IdMateria
+INNER JOIN aluno   ON aluno.IdAluno     = trabalho.IdAluno
+
+-- Deixamos somente os dados que nos interessam no momento
+SELECT 
+	aluno.Nome,
+	aluno.Ra,
+	aluno.Idade,
+	materia.Materia,
+	trabalho.Nota
+FROM trabalho
+INNER JOIN materia ON materia.IdMateria = trabalho.IdMateria
+INNER JOIN aluno   ON aluno.IdAluno     = trabalho.IdAluno
+
+/* 
+	Utilizamos a função nativa COUNT ]
+	para retornar o numero de registros na minha tabela
+
+	Usamos o Alias "AS" para nomer a coluna
+*/
+SELECT COUNT(*) AS total_registros FROM aluno
+
+/* Pegamos a idade máxima dos alunos */
+SELECT MAX(Idade) AS idade_maxima FROM aluno
+
+/*Pegamos a idade mínima do alunos*/
+SELECT MIN(Idade) AS idade_minima FROM aluno
+
+/* pegamos a média de idade dos alunos */
+SELECT AVG(Idade) AS meida_de_idades FROM aluno
+
+/* Somamos as idades */
+SELECT SUM(Idade) AS soma_de_idades FROM aluno
+
+/* Juntamos todas as funções em uma unica consulta */
+SELECT
+	COUNT(*) AS total,
+	MAX(Idade) AS idade_maxima,
+	MIN(Idade) AS idade_minima,
+	AVG(Idade) AS media_idade,
+	SUM(Idade) AS soma_idade
+FROM aluno
+
+/* SUBSTRING */
+-- pegamos parte de uma string
+SELECT SUBSTRING(Nome, 0, 2) AS inicial FROM aluno
+
+/* UPPER */
+-- Deixar tudo em maiúsculo
+SELECT UPPER(Nome) AS Nome FROM aluno
+
+/* LOWER */
+-- Deixar tudo minusculo
+SELECT LOWER(Nome) AS Nome FROM aluno
+
